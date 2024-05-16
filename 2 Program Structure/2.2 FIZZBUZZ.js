9b9404ab709f27fe4dba6b7a64e1077d1fc60c64
ahmedwaelprogrammer <aw88301@gmail.com>
2024-05-16 16:05:10 +0300

    Chapter 2 done

---
+++ 2 Program Structure/2.2 FIZZBUZZ.js
@@ -0,0 +1,15 @@
+﻿for (let count = 0; count < 101; count++) {
+
+    if (count % 5 == 0 && count % 3 != 0) {
+        console.log("BUZZ");
+    }
+    else if (count % 5 == 0 && count % 3 == 0) {
+        console.log("FIZZBUZZ");
+    }
+    else if (count % 3 == 0) {
+        console.log("FIZZ");
+    }
+    else {
+        console.log(count);
+    }
+}
---
+++ 2 Program Structure/2.3 chessBoard.js
@@ -0,0 +1,12 @@
+﻿let XSize = 8;
+let YSize = 8;
+let CHR = '# ';
+for (let count = 0; count < YSize; count++) {
+    let seq = ""
+    if (count % 2 == 0) seq += " ";
+    else seq += "";
+    for (let XCount = 0; XCount < XSize; XCount++) {
+        seq += CHR;
+    }
+    console.log(seq)
+}
